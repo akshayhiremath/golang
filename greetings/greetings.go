fd3338eb397f00f4427b1e641c1a4a34d4f633bf
Akshay Hiremath <akshayhiremath@gmail.com>
2022-02-21 00:16:44 -0500

    Add error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	//Check if name is empty and return error
+	if name == "" {
+		return "", errors.New("empty name entry")
+	}
+
+	//If name has a value return a greeting message
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	greetings "github.com/akshayhiremath/golang/greetings"
 )
 
 func main() {
 
-	//fmt.Println("Hello, World!")
-	message := greetings.Hello("Akshay")
+	//Add logger with initial config to set prefix and disable timestamp, source file and line number
+	log.SetPrefix("Greetings: ")
+	log.SetFlags(0)
+
+	//Send empty value
+	message, err := greetings.Hello("")
+
+	//Log error
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//If non empty name is sent to greetings provider. Print a greeting with the name to console
 	fmt.Println(message)
 }
